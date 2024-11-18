21e2890dccb9d0cfb6ceca109056899b2d3c5f51
aloHyomora <alohomoratolit@naver.com>
2024-11-18 23:17:44 +0900

    [Feat] : 탐지된 객체 시각화

--- YOLOv8/Yolo Test/yolo_test.py
+++ YOLOv8/Yolo Test/yolo_test.py
@@ -1,15 +1,20 @@
 from ultralytics import YOLO
+import cv2
 
 # YOLOv8n 모델 불러오기 : 사전 학습된 모델
 model = YOLO('yolov8n.pt')
 
 # 이미지 경로 설정
-image_path = 'C:\\Users\\aloho\\Github\\Binding-AI-XR-Alohyomora\\YOLOv8\\Yolo Test\\fruit.jpg'
+image_path = 'C:\\Users\\aloho\\Github\\Binding-AI-XR-Alohyomora\\YOLOv8\\Yolo Test\\dog.jpg'
+
+# 이미지 로드
+img = cv2.imread(image_path)
 
 # 객체 탐지 수행
 results = model(image_path)
 
-# 결과 시각화
+# 감지된 객체를 이미지에 표시
+annotated_img = results[0].plot()
 
 for result in results:
     boxes = result.boxes  # 바운딩 박스 정보
@@ -18,3 +23,13 @@ for result in results:
         conf = box.conf  # 신뢰도
         xyxy = box.xyxy  # 바운딩 박스 좌표
         print(f'클래스: {cls}, 신뢰도: {conf}, 좌표: {xyxy}')
+        
+        
+# 결과 이미지 저장
+output_path = 'annotated_image.jpg'
+cv2.imwrite(output_path, annotated_img)
+        
+# 결과 이미지 표시
+cv2.imshow('Detected Objects' , annotated_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
